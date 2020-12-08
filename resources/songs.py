beadac65e0b9071025e816d1fd929c3bb9e53af4
Marc Wright <marc@generalassemb.ly>
2020-12-08 10:55:03 -0600

    add edit delete and show to songs

--- resources/songs.py
+++ resources/songs.py
@@ -23,3 +23,26 @@ def create_songs():
     song = models.Song.create(**payload)
     song_dict = model_to_dict(song)
     return jsonify(data=song_dict, status={"code": 201, "message": "Success"})
+
+@song.route('/<id>', methods=["GET"])
+def get_one_song(id):
+    try:
+      print(id, 'reserved word?')
+      song = models.Song.get_by_id(id)
+      print(song.__dict__)
+      return jsonify(data=model_to_dict(song), status={"code": 200, "message": "Success"})
+    except models.DoesNotExist:
+      return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
+
+@song.route('/<id>', methods=["PUT"])
+def update_song(id):
+    payload = request.get_json()
+    query = models.Song.update(**payload).where(models.Song.id==id)
+    query.execute()
+    return jsonify(data=model_to_dict(models.Song.get_by_id(id)), status={"code": 200, "message": "resource updated successfully"})
+
+@song.route('/<id>', methods=["Delete"])
+def delete_song(id):
+    query = models.Song.delete().where(models.Song.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status={"code": 200, "message": "resource deleted successfully"})
